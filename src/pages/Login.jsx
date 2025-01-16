6d7aa09ee222bda6b9ee4d1c96e49fc2d605feb6
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-17 03:12:57 +0530

    added login page and edited some changes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,47 @@
+const Login = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
+        <h2 className="text-2xl pt-10 font-semibold text-center">
+          Login to your account
+        </h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none hover:bg-slate-300 hover:text-gray-800">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
